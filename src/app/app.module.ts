10616660ef899c28bf05ddfbd7ed5c86e653f9a1
Irwin Herridge <irwinherridge3@gmail.com>
2020-11-24 16:26:28 +0200

    Implemented the basic routes component.. also implemented a error component and added the default and error routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { FooterComponent } from './footer/footer.component';
 import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
     FooterComponent,
-    LoginComponent
+    LoginComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  username = 'in28minutes';
+  password = '';
+  errorMessage = 'Invalid Login';
+  invalidLogin = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  handleLogin() {
+    if (this.username === '')
+      this.invalidLogin = true;
+    else
+      this.invalidLogin = false;
+  }
 }
