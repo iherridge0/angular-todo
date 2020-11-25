da22c0fa01231c7cd7f3dc0f846a7bd6aedc256a
Irwin Herridge <irwinherridge3@gmail.com>
2020-11-25 17:13:06 +0200

    Learned how to use Bootstrap to build responseive, mobile-first sites

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { FooterComponent } from './footer/footer.component';
 import { LoginComponent } from './login/login.component';
 import { ErrorComponent } from './error/error.component';
 import { ListTodosComponent } from './list-todos/list-todos.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { ListTodosComponent } from './list-todos/list-todos.component';
     FooterComponent,
     LoginComponent,
     ErrorComponent,
-    ListTodosComponent
+    ListTodosComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
